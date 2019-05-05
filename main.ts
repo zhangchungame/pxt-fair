4e1d6b3d80cebfa273f8976ff2b10dee3961ecf1
zc <zhangchungame@163.com>
2019-05-06 01:37:41 +0800

    sdf

--- main.ts
+++ main.ts
@@ -140,6 +140,72 @@ namespace BH1750 {
     export function Ultrasonic_Car(): number {
         return 1;
     }
+    //% blockId=BH1750_getArg block="获取变量2|pin %pin|pin2 %pin|pin3 %pin|pin4 %pin|pin5 %pin|pin6 %pin|pin7 %pin|pin8 %pin"
+    //% weight=5
+    //% blockGap=8
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
+    export function getArg(pin: DigitalPin,pin2: DigitalPin,pin3: DigitalPin,pin4: DigitalPin,pin5: DigitalPin,pin6: DigitalPin,pin7: DigitalPin,pin8: DigitalPin): number {
+        if(pins.digitalReadPin(pin5)==1){
+            return 1;
+        }
+        if(pins.digitalReadPin(pin6)==1){
+            return 2;
+        }
+        if(pins.digitalReadPin(pin7)==1){
+            return 3;
+        }
+        if(pins.digitalReadPin(pin8)==1){
+            return 4;
+        }
+        pins.digitalWritePin(pin, 0);
 
+        pins.digitalWritePin(pin2, 1);
+        if(pins.digitalReadPin(pin5)==1){
+            return 5;
+        }
+        if(pins.digitalReadPin(pin6)==1){
+            return 6;
+        }
+        if(pins.digitalReadPin(pin7)==1){
+            return 7;
+        }
+        if(pins.digitalReadPin(pin8)==1){
+            return 8;
+        }
+        pins.digitalWritePin(pin2, 0);
+
+
+        pins.digitalWritePin(pin3, 1);
+        if(pins.digitalReadPin(pin5)==1){
+            return 9;
+        }
+        if(pins.digitalReadPin(pin6)==1){
+            return 10;
+        }
+        if(pins.digitalReadPin(pin7)==1){
+            return 11;
+        }
+        if(pins.digitalReadPin(pin8)==1){
+            return 12;
+        }
+        pins.digitalWritePin(pin3, 0);
+
+        pins.digitalWritePin(pin4, 1);
+        if(pins.digitalReadPin(pin5)==1){
+            return 13;
+        }
+        if(pins.digitalReadPin(pin6)==1){
+            return 14;
+        }
+        if(pins.digitalReadPin(pin7)==1){
+            return 15;
+        }
+        if(pins.digitalReadPin(pin8)==1){
+            return 16;
+        }
+        pins.digitalWritePin(pin4, 0);
+        return 0;
+    }
 }
  
